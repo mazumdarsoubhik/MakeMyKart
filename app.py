b1ecbf8a03df587b1ca8b66952613566333b3730
Soubhik Mazumdar <44722027+mazumdarsoubhik@users.noreply.github.com>
2023-06-25 16:26:45 +0000

    alpha-2.0

--- app.py
+++ app.py
@@ -1,8 +1,37 @@
 from flask import Flask, render_template, request
 from model import LLM
+import json
 
 app=Flask(__name__)
 
+def flatten_list(nested_list):
+    return [item for sublist in nested_list for item in sublist]
+
 @app.route("/", methods=["GET", "POST"])
 def home_page():
+
     return render_template('index.html', url="https://via.placeholder.com/400", headline="User Preference")
+
+@app.route("/dish", methods=["GET"])
+def menu_page():
+    if request.method == "GET":
+        user_preference = request.args.get("user_preference")
+        print("User Preference :: ", user_preference)
+        dish_list = LLM.get_dish_suggestion(user_preference)
+        data = {"status": 200, "description": "Correct specification", "data": json.dumps(dish_list) }
+        return json.dumps(data)
+    else:
+        return {"status": 404, "description": "Please specify your preference correctly"}
+
+@app.route("/ingredient", methods=["GET"])
+def ingredients_page():
+    # i/p : [{"dish_name": abd, "serves": 3}, {"dish_name":bsa, "serves":3}]
+    selected_menu = request.form["selected_menu"]
+    selected_menu = list(selected_menu)
+    ingredient_list=[]
+    for dish in selected_menu:
+        ingredients_data = LLM.get_ingredients(dish)
+        ingredient_list.extend(ingredients_data)
+    data = {"status": 200, "description": "Correct specification", "data": json.dumps(ingredient_list) }
+    return json.dumps(data)
+    
--- model/LLM.py
+++ model/LLM.py
@@ -5,12 +5,14 @@ service = UseLLM(service_url="https://usellm.org/api/llm")
 
 def get_dish_suggestion(user_input):
     sys_property='''You're a bot that will help user by suggesting dishes of their preference. 
-    The user will give you their preference as text and according to that you have to suggest them dishes with details.
-    The output must be in list of JSON objects and should look as: [{ "dish_name": {{ dish_name }}, "dish_description": {{ dish_description }}, "dish_cooking_time": {{ dish_cooking_time }}, "dish_calories": {{ dish_calories }} }]
-    If user is not specific to a single dish then only suggest more than 10 dishes.
+    The user will give you their preference as text and according to that you have to suggest them dishes with details. You're a bot which gives output in specified format, do not give conversational output.
+    Ouput format (List of JSONs): [{ "dish_name": {{ dish_name }}, "dish_description": {{ dish_description }}, "dish_cooking_time": {{ dish_cooking_time }}, "dish_calories": {{ dish_calories }} }]
+    If user is not specific to a single dish then only suggest more than 3 and less than 5 dishes.
+    You are in no condition going to give paragraph output you must return output as LIST of JSONs format specified above.
+    Do not put any extra words other than LIST of JSON.
     '''
-    user_example_ip = '''I'm bengali and I've family of size 3. We prefer our native food majorly but we also prefer south indian sometimes'''
-    assistant_example_op='''{ "dish_name": "Aloo Poshto", "dish_description": "A vegetarian dish made with potatoes and poppy seeds. The poppy seed paste gives it a unique flavor and a slight nuttiness.", "dish_cooking_time": "40 mins", "dish_calories": "200" }'''
+    user_example_ip = '''I'm bengali and I've family of size 3. We prefer our native food majorly but we also prefer north indian sometimes'''
+    assistant_example_op='''[{ "dish_name": "Aloo Poshto", "dish_description": "A vegetarian dish made with potatoes and poppy seeds. The poppy seed paste gives it a unique flavor and a slight nuttiness.", "dish_cooking_time": "40 mins", "dish_calories": "200" },{ "dish_name": "Kosha Mangsho", "dish_description": "A Bengali dish made with tender mutton pieces cooked in a rich and spicy gravy.", "dish_cooking_time": "90 mins", "dish_calories": "450" },{ "dish_name": "Chole Bhature", "dish_description": "A classic North Indian dish consisting of spicy chickpeas (chole) served with deep-fried bread (bhature).", "dish_cooking_time": "60 mins", "dish_calories": "500" }]'''
 
     # Prepare the conversation
     messages = [
@@ -32,7 +34,7 @@ def get_dish_suggestion(user_input):
     return data
 
 def get_ingredients(dish_details):
-    sys_property='''You'll be given a JSON object which will contain a food dish name as "dish_name" and number of people that are going to consume it as "serves".
+    sys_property='''You'll be given a list of JSON object which will contain a food dish name as "dish_name" and number of people that are going to consume it as "serves". You're bot which gives output in specified format, do not give conversational output.
     You need to provide ingredients needed to cook the dish at home. Only suggest those ingredients that the person needs to buy.
     Input format will be like: { "dish_name": {{ dish_name }}}, "serves": {{ serves }} }
     Output format should be a list of JSONs containing item details like: [{"item": {{ item }}, "category": {{ category }}, "quantity": {{ quantity }}, "unit": {{ unit }} }]
@@ -61,10 +63,10 @@ def get_ingredients(dish_details):
     return data
 
 if __name__ == '__main__':
-	# I_want = 'Vegeterian dishes only'
+	# I_want = 'I prefer south indian and north indian food'
 	# values = get_dish_suggestion(I_want)
 	# print(values)
 
-    dish_detail = '{ "dish_name": "Butter Chicken Masala", "serves": 3 }'
+    dish_detail = '{ "dish_name": "Butter Chicken", "serves": 3 }'
     ingredients_values = get_ingredients(dish_detail)
     print(ingredients_values)
